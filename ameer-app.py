a5094d9b7f45d024cdb301e55bd3a961ce10775a
Aryaman Singh Dev <aryaman.s.dev@gmail.com>
2021-01-11 21:16:38 +0530

    Update ameer-app.py

--- ameer-app.py
+++ ameer-app.py
@@ -1,19 +1,19 @@
 # Short code
 import tweepy
-consumer_key = ''
-consumer_secret = ''
-access_token = '-'
-secret_access_token = ''
+api_key = 'IKB2Ce7CSVPTBC62ElYPfgGQV'
+api_secret_key = 'CFDmCz2CVvxyXSUYqFodmvgx52EIYhCT8GhffXDQTEtsuyO2X4'
+access_token = '1303659259079487488-iGOgIdTFJ7fPlM0hyfo8Ej5BpR32m3'
+secret_access_token = 'IE8ab5VFIanZzhYrSleWORrxGrKfHALSbtrMmFif9TeyC'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(api_key, api_secret_key)       #for authentication 
 auth.set_access_token(access_token, secret_access_token)
 api = tweepy.API(auth)
 print('Everything is fine')
 
-import time
-while True:
-  user = api.get_user('Ameen91741779')
-  f = user.followers_count
-  api.update_profile(name=f'AMEER {f} Followers')
-  print(f'AMEER {f} Followers')
-  time.sleep(60)
+import time 
+while True: 
+  user = api.get_user('AD_bxd')
+  follower = user.followers_count 
+  api.update_profile(name = f'ARYAMAN {follower} followers')
+  print(f'ARYAMAN {follower} followers')
+  time.sleep(60)                      #but this wont work if the program is not running all the time 
